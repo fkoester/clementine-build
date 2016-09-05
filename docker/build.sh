811ca7189750e2808100fb9446a6a0ff45c29973
Fabian Köster <mail@fabian-koester.com>
2016-09-05 07:21:09 +0200

    Fail in case of errors

--- docker/build.sh
+++ docker/build.sh
@@ -1,22 +1,29 @@
 #! /bin/bash
 
+function die {
+    echo >&2 "$@"
+    exit 1
+}
+
 REPO=${1:-https://github.com/clementine-player/Clementine.git}
 BRANCH=${2:-master}
 
-git clone $REPO clementine-player
-cd clementine-player/bin
-git checkout $BRANCH
+git clone $REPO clementine-player || die "git clone failed"
+cd clementine-player/bin || die
+git checkout $BRANCH || die "git checkout failed"
 
 PKG_CONFIG_LIBDIR=/target/lib/pkgconfig \
   cmake .. \
   -DCMAKE_TOOLCHAIN_FILE=/src/Toolchain-mingw32.cmake \
   -DQT_HEADERS_DIR=/target/include \
   -DQT_LIBRARY_DIR=/target/bin \
-  -DPROTOBUF_PROTOC_EXECUTABLE=/target/bin/protoc && \
-  make
+  -DPROTOBUF_PROTOC_EXECUTABLE=/target/bin/protoc \
+  || die "cmake failed"
 
-cd clementine-player/dist/windows
+make || die "make failed"
+
+cd clementine-player/dist/windows || die
 
 ln -s /src/windows/clementine-deps/* . && \
 ln -s ../../bin/clementine*.exe . && \
-makensis clementine.nsi
+makensis clementine.nsi || die "makensis failed"
